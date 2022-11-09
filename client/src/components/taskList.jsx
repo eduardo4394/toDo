97ce237f6b0938a21a8fe268484018e1b7a70e9e
eduardo salinas <eduardo_sh@outlook.com>
2022-11-09 11:16:37 -0500

    fixing 2

--- client/src/components/taskList.jsx
+++ client/src/components/taskList.jsx
@@ -1,7 +1,77 @@
+import { Button, Card, CardContent, Typography } from "@mui/material";
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+
 export default function TaskList() {
+  const navigate = useNavigate();
+
+  const [tasks, setTasks] = useState([]);
+
+  const loadTasks = async () => {
+    const res = await fetch("http://localhost:3001/tasks");
+    const data = await res.json();
+    setTasks(data);
+  };
+
+  const handleDelete = async (id) => {
+    try {
+      const resp = await fetch(`http://localhost:3001/tasks/${id}`, {
+        method: "DELETE",
+      });
+      setTasks(tasks.filter((task) => task.id !== id));
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    loadTasks();
+  }, []);
   return (
-    <div>
-      <h1>Task list</h1>
-    </div>
+    <>
+      <h1>Tasks List</h1>
+      {tasks?.map((task) => {
+        return (
+          <Card
+            key={task.id}
+            style={{
+              marginBottom: "0.75rem",
+              backgroundColor: "#1e272e",
+              color: "white",
+            }}
+          >
+            <CardContent
+              style={{
+                display: "flex",
+                flexWrap: "wrap",
+                justifyContent: "space-between",
+                alignItems: "center",
+              }}
+            >
+              <div>
+                <Typography>{task.title}</Typography>
+                <Typography>{task.description}</Typography>
+              </div>
+              <div style={{ display: "flex", gap: "0.5rem" }}>
+                <Button
+                  variant="contained"
+                  color="secondary"
+                  onClick={() => navigate(`/tasks/${task.id}/edit`)}
+                >
+                  Edit
+                </Button>
+                <Button
+                  variant="contained"
+                  color="warning"
+                  onClick={() => handleDelete(task.id)}
+                >
+                  Delete
+                </Button>
+              </div>
+            </CardContent>
+          </Card>
+        );
+      })}
+    </>
   );
 }
