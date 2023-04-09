a8de437d9ed54206b3928c11a8469d31d56a5814
eduardo salinas <eduardo_sh@outlook.com>
2023-04-08 23:51:20 -0500

    fix

--- server/db.js
+++ server/db.js
@@ -1,20 +1,27 @@
 const { Pool } = require("pg");
 // const { config } = require("dotenv");
 // config();
-
-// const pool = new Pool({
-//   user: process.env.DB_USER,
-//   password: process.env.DB_PASSWORD,
-//   host: process.env.DB_HOST,
-//   port: process.env.DB_PORT,
-//   database: process.env.DB_DATABASE,
-// });
-
-const connectionString =
-  "PGPASSWORD=TpYxL8a7sZN2SR3pyvjs psql -h containers-us-west-117.railway.app -U postgres -p 6829 -d railway";
+import {
+  DB_HOST,
+  DB_PORT,
+  DB_PASSWORD,
+  DB_USER,
+  DB_DATABASE,
+} from "./config.js";
 
 const pool = new Pool({
-  connectionString,
+  user: DB_USER,
+  password: DB_PASSWORD,
+  host: DB_HOST,
+  port: DB_PORT,
+  database: DB_DATABASE,
 });
+
+// const connectionString =
+//   "PGPASSWORD=TpYxL8a7sZN2SR3pyvjs psql -h containers-us-west-117.railway.app -U postgres -p 6829 -d railway";
+
+// const pool = new Pool({
+//   connectionString,
+// });
 
 module.exports = pool;
